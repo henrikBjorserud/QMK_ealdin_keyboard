aaf43d8a151111a20a34012491cb5e824972d224
henrik björserud <henrikbjorserud@gmail.com>
2023-05-10 13:59:40 +0200

    fixed wrong button

--- ealdin/quadrant/keymaps/henrikBjorserud/keymap.c
+++ ealdin/quadrant/keymaps/henrikBjorserud/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       * `-------------------------------------------------------------------------------------------------' 
       */
     KC_MUTE, KC_1,   KC_2,    KC_3,    KC_4,    KC_5,    KC_HOME, KC_END,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_SLASH, KC_BSLS, KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_SLASH,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_SLASH, KC_BSLS, KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC,
     KC_ESC,  HOME_A,  HOME_S,  HOME_D,  HOME_F,  KC_G,    KC_LBRC, KC_RBRC, KC_H,    HOME_J,  HOME_K,  HOME_L,  HOME_SCLN, KC_QUOT,
     LSFT(KC_9), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MINS, KC_EQL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   RSFT(KC_0),
     KC_LCTL, KC_LGUI, KC_CAPS, KC_LALT, MO(2),   KC_BSPC, KC_BSPC, KC_SPC,  KC_SPC,  KC_ENT,  LCTL(KC_TAB),   KC_LEFT, KC_DOWN, KC_RGHT
